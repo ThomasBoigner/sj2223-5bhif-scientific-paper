265cd80532dbc5956b22564c6f2241643fe17da6
Thomas Boigner <BOI19439@spengergasse.at>
2022-08-17 15:54:40 +0200

    Initial commit
    Add spring-boot project with dependencies

---
+++ src/main/java/at/spengergasse/sj22235bhifpos1scientificpaper/Sj22235bhifPos1ScientificPaperApplication.java
@@ -0,0 +1,13 @@
+package at.spengergasse.sj22235bhifpos1scientificpaper;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Sj22235bhifPos1ScientificPaperApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Sj22235bhifPos1ScientificPaperApplication.class, args);
+    }
+
+}
---
+++ src/test/java/at/spengergasse/sj22235bhifpos1scientificpaper/Sj22235bhifPos1ScientificPaperApplicationTests.java
@@ -0,0 +1,13 @@
+package at.spengergasse.sj22235bhifpos1scientificpaper;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Sj22235bhifPos1ScientificPaperApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
